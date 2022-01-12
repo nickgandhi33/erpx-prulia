fbe41f5252c949d23683dccd98aa0837c0173b72
nickgandhi33 <gandhi.nikhil3@gmail.com>
2022-01-12 14:30:30 +0530

    Update member_registration.py

--- erpx_prulia/prulia_members/web_form/member_registration/member_registration.py
+++ erpx_prulia/prulia_members/web_form/member_registration/member_registration.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 import frappe
-from frappe.utils import getdate, validate_email_add, today
+from frappe.utils import getdate, validate_email_address, today
 def get_context(context):
 	# do your magic here
 	pass
