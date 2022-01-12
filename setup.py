3740b99167b155687e7976c93eda4f59ef579200
nickgandhi33 <gandhi.nikhil3@gmail.com>
2022-01-12 13:06:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,5 +28,5 @@ setup(
 	include_package_data=True,
 	install_requires=[str(ir.requirement) for ir in install_reqs]
 	# install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	# dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
